6b9bb66e4b7fc7b29f526982fa081e86b7692437
ditorizkyka <rizkykandito@gmail.com>
2026-03-07 14:31:23 +0700

    Initial Commit

---
+++ Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by Andito Rizkyka Rianto on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
