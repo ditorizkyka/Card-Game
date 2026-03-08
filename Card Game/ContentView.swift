5018f3199006cbde00bbb699da0034cb5755b243
ditorizkyka <rizkykandito@gmail.com>
2026-03-08 15:32:34 +0700

    Slicing the main screen

--- Card Game/ContentView.swift
+++ Card Game/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack() {
+            Image("background-plain").ignoresSafeArea()
+            
+            
+            VStack {
+                Spacer()
+                //               LOGO
+                Image("logo")
+                Spacer()
+                
+                HStack(spacing :50) {
+                    
+                    //                    CARD  1
+                    Image("back")
+                    
+                    Image("card2")
+                    
+                }
+                Spacer()
+                Image("button")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline).padding(.bottom)
+                            
+                        Text("0")
+                            .font(.largeTitle)
+                            
+                        
+                    }.foregroundStyle(Color(.white))
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline).padding(.bottom)
+                            
+                        Text("0")
+                            .font(.largeTitle)
+                            
+                    }.foregroundStyle(Color(.white))
+                    Spacer()
+                }
+                Spacer()
+                
+            }
+            
         }
-        .padding()
+        
     }
 }
 
